8944b7555175c3d454a296d14c9fa2ec6b8d6885
Ankrick <tn8070250@gmail.com>
2025-04-03 00:04:15 +0700

    vercel link fix

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 app.use(cors(
     {
-        origin : "https://quake-track.vercel.app/",
+        origin : "https://quake-track.vercel.app",
         credentials : true
     }
 ));
