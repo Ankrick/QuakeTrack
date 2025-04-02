47e87abe389b74d365abcd6acbe6854aa9cceaeb
Ankrick <tn8070250@gmail.com>
2025-04-03 00:01:34 +0700

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 app.use(cors(
     {
-        origin : "http://localhost:5173",
+        origin : "https://quake-track.vercel.app/",
         credentials : true
     }
 ));
