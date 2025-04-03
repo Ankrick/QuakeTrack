c3eb5b1746554d4e1206e34b164184c8fe7fef6c
Ankrick <tn8070250@gmail.com>
2025-04-03 14:09:52 +0700

    analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar.jsx"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <div className="p-5 h-screen">
         <Outlet/>
+        <Analytics/>
       </div>
     </>
   )
